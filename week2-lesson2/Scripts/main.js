dc292abe8d9455d51312a66d69b59526a1a06a78
ToddR83 <toddraymond_0@hotmail.com>
2014-09-11 16:49:09 -0400

    Added JQuery.  Issue arose.  Unable to print to console?

--- week2-lesson2/Scripts/main.js
+++ week2-lesson2/Scripts/main.js
@@ -1,1 +1,15 @@
-﻿
+﻿/// <reference path="jquery-2.1.1.min.js" />
+
+
+
+if (jQuery)
+{
+    $("#lesson-header").show();
+
+    var paraString = $("#lesson-header p").text();
+
+    console.debug(paraString);
+    console.log(paraString);
+}
+
+
